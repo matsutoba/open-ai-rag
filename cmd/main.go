5defed0d3c59ad432c5fc637d04c0bcf8cd7768d
Toshiyuki Matsubara <matsubara_t@outlook.jp>
2025-03-01 10:59:10 +0900

    Webページをスクレイピングして質問に回答するRAG

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"open-ai-rag/internal/config"
+	"open-ai-rag/pkg/utils"
+	"open-ai-rag/samples"
+
+	"github.com/sashabaranov/go-openai"
 	// imported as openai
 )
 
 func main() {
-	config.LoadEnv()
+	utils.LoadEnv()
+	apiKey := utils.GetEnv("OPEN_AI_API_KEY", "")
+	client := openai.NewClient(apiKey)
 
-	apiKey := config.GetEnv("OPEN_AI_API_KEY", "")
-	fmt.Println(apiKey)
+	url := "質問をしたいページのURL"
+	question := "質問文"
+	samples.WebTextQandA(client, url, question)
 }
